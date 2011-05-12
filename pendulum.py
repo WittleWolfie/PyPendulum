d7ebbcea17605707218dd2c99466a6015d682640
Tyler Goeringer <tyler.goeringer@gmail.com>
2011-05-12 00:27:49 -0400

    Fixed a bug with the 90 degree sanity check. Refactored update().

--- pendulum.py
+++ pendulum.py
@@ -21,6 +21,8 @@ m = 2.0
 M = 8.0
 l = 0.5
 a = 1.0/(m+M)
+epsilon = 0.000001
+import pdb
 
 class Pendulum:
     # Initially the pendulum is set to a slightly unstable state near (x,v) = (0, 0)
@@ -32,17 +34,18 @@ class Pendulum:
     # IMPORTANT NOTE: This is an approximate update step, with error proportional to the size of dx and
     # the angular velocity and acceleration.
     def update(self, dt, u):
-        accel = (g*math.sin(self.x) - a*m*l*self.v*self.v*math.sin(2*self.x)/2 - a*math.cos(self.x)*u)
-        accel = accel/(4*l/3 - a*m*l*math.pow(math.cos(self.x), 2))
-        
         self.x += self.v*dt
-        self.v += accel*dt
         
         # Check if we have hit 90 degrees, if so we are stable
-        if self.x > math.pi/2:
+        if self.x - math.pi/2 >= -epsilon:
             self.x = math.pi/2
             self.v = 0
-        elif self.x < -math.pi/2:
+        elif self.x + math.pi/2 <= epsilon:
             self.x = -math.pi/2
             self.v = 0
+        else:
+            accel = (g*math.sin(self.x) - a*m*l*self.v*self.v*math.sin(2*self.x)/2 - a*math.cos(self.x)*u)
+            accel = accel/(4*l/3 - a*m*l*math.pow(math.cos(self.x), 2))
+            self.v += accel*dt
             
+pdb.set_trace()
