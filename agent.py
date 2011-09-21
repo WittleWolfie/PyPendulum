6a8423a8f76067b8e75fe858a2c29f798e05c41c
Tyler Goeringer <tyler.goeringer@gmail.com>
2011-09-20 21:31:04 -0400

    Added random behavior to agent, added debug print statement to pendulum, and created simple scenario for simulation.

--- agent.py
+++ agent.py
@@ -2,12 +2,15 @@
     LSPI-Learning Agent for use in PyPendulum.
 """
 
+from random import *
+
 class Agent:
-    def __init__(self):
-        pass
-        
-    def getAction(self, x, v):
-        pass
-        
-    def notify(self, reward):
-        pass
+	def __init__(self):
+		pass
+		
+	def getAction(self, x, v):
+		return choice([-50, 50]) # Right now just return a random choice
+	 
+	def notify(self, reward):
+		pass # Don't give a crap.
+		
--- simulation.py
+++ simulation.py
@@ -1,10 +1,13 @@
 """
-    Main class for PyPendulum. Uses agent.py to attempt an optimal solution to
-    the inverted pendulum problem.
+	Main class for PyPendulum. Uses agent.py to attempt an optimal solution to
+	the inverted pendulum problem.
 """
 
 from pendulum import *
 from agent import *
+
+def calculateReward():
+	return 1
 
 # Time between updates
 dt = 0.1
@@ -12,5 +15,6 @@ dt = 0.1
 pen = Pendulum()
 agent = Agent()
 while(pen.isHorizontal() == False):
-    action = agent.getAction(pen.x, pen.v)
-    agent.notify(calculateReward())
+	action = agent.getAction(pen.x, pen.v)
+	pen.update(0.1, action)
+	agent.notify(calculateReward())
